c7b75ea37139cf58a0da649311f3e9a6a9fc87fe
Becka McNally <beckamcnally@gmail.com>
2023-02-20 06:02:42 -0800

    retried notes from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,50 +5,37 @@ $(function () {
 var currentDay = $("#currentDay");
 var btn = $(".btn");
 var timeBlock = $('.time-block')
+var notes;
   
   // event listener for click of save button captures text value and send it to local storage wit the id for the corresponding container.
   btn.on("click", function () {
-    var cureentTimeBlock = $(this).parent().attr('id'); 
+    var clickedTimeBlock = $(this).parent().attr('id'); 
     var noteDescription = $(this).siblings('textarea').val()
-   
-    var newNote = {
-      cureentTimeBlock: cureentTimeBlock,
-      text: noteDescription
-    }
- // TODO: Add code to get any user input that was saved in localStorage and set
+    // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-
-
-
-    var description= readFromStorage()
-    description.push(newNote)
-    saveToStorage(description)
    
-    return description;
+    console.log(clickedTimeBlock)
+    console.log(noteDescription)
+    localStorage.setItem(clickedTimeBlock, noteDescription)
   });
 
-  function readFromStorage() {
-    var description = localStorage.getItem('description')
-    console.log(description)
-    if (description) {
-      description = JSON.parse(description);
-    } else {
-      description = [];
-    }
-    return description;
+ for (var i = 0; i < timeBlock.length; i++) {
+  var element = timeBlock[i];
+  var noteKey = element.id
+  var storedNote = localStorage.getItem(noteKey)
+  var noteContainer = $(element).children('textarea')
+  if(element === null){
+    return;
+  } else {
+    noteContainer.append(storedNote)
   }
+ }
 
-   function saveToStorage(description) {
-    localStorage.setItem("description", JSON.stringify(description))
-    
-  }
  
   // displays past present and future
   var currentHour = dayjs.format("H")
   
-
-
   for (var i = 0; i < timeBlock.length; i++) {
     var element = timeBlock[i];
     var elementHour = element.id
@@ -61,8 +48,7 @@ var timeBlock = $('.time-block')
     } else {
       $(element).removeClass('present past').addClass('future')
     }
-
-   }
+  }
 
   // Adds code to display the current date in the header of the page.
   currentDay.text(dayjs.format("dddd, MMMM, D, YYYY"));
--- req.md
+++ req.md
@@ -25,7 +25,7 @@ WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours✔️
 
 WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future✔️
 
 WHEN I click into a timeblock
 THEN I can enter an event ✔️
